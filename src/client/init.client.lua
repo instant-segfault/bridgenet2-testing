d9b2215856f1657faa00867c805b2c869d044484
instant-segfault <bashguy10@gmail.com>
2023-04-19 16:05:00 -0700

    bridgenet

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,1 +1,15 @@
-print("Hello world, from client!")
+local BridgeNet2 = require(game.ReplicatedStorage.Packages.BridgeNet2)
+
+-- The name string must be the same as the one on the server.
+local bridge = BridgeNet2.ReferenceBridge("Example")
+
+-- We will connect to the event just like we did on the server.
+bridge:Connect(function(args)
+    print("Received event from server")
+	for _, argument in args do
+        print(argument)
+    end
+end)
+
+-- Like normal, send our data to the server.
+bridge:Fire({"Hello", "Server"})
--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,3 +1,26 @@
-local HttpGetWrapper = require(game.ReplicatedStorage.Common.HttpGetWrapper)
+local BridgeNet2 = require(game.ReplicatedStorage.Packages.BridgeNet2)
 
-HttpGetWrapper("https://cat-fact.herokuapp.com/facts"):andThen(print)
+-- Calling ReferenceBridge() will return either a ServerBridge or ClientBridge metatable depending on the enviroment.
+local bridge = BridgeNet2.ReferenceBridge("Example")
+
+-- We can specify how many requests this bridge can receive per second.
+bridge:RateLimit(10, function(player)
+	-- Setting a rate limit also allows you to have a callback function.
+    -- If this function returns true, then the rate limit will be ignored.
+	if player.Name == "Roblox" then
+        return true
+	end
+    print(player.Name .. " triggered the ratelimiter")
+end)
+
+-- We can connect to events sent from clients like normal, with one difference, arguments are now defined in a table, instead of multiple variables.
+bridge:Connect(function(player, args)
+    print("Received event from "..player.Name)
+	for _, argument in args do
+        print(argument)
+    end
+
+    -- Send player's ID and their sent arguments back to all clients.
+    -- BridgeNet2 has multiple utility functions for getting players, see the documentation.
+    bridge:Fire(BridgeNet2.AllPlayers(), {player.UserId, unpack(args)})
+end)
